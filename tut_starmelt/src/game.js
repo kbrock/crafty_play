2cd6d8476461504204acb9732b01602cf1193b5a
Darren Torpey <darren.torpey@gmail.com>
2012-11-22 00:05:23 -0500

    cleanup

--- tut_starmelt/src/game.js
+++ tut_starmelt/src/game.js
@@ -1,6 +1,5 @@
 
 
-
 Crafty.c('Box', {
   init: function() {
     this.addComponent('2D, Canvas, Color');
@@ -145,73 +144,18 @@ window.onload = function() {
   // Start crafty
   Crafty.init(400, 320);
 
-  // Crafty.canvas();
-
   // Turn the sprite map into usable components
   Crafty.sprite(16, 'assets/crafty-sprite.png', {
-    grass1: [0,0],
-    grass2: [1,0],
-    grass3: [2,0],
-    grass4: [3,0],
-    flower: [0,1],
-    bush1:  [0,2],
-    bush2:  [1,2],
-    player: [0,3]
+    grass1: [0, 0],
+    grass2: [1, 0],
+    grass3: [2, 0],
+    grass4: [3, 0],
+    flower: [0, 1],
+    bush1:  [0, 2],
+    bush2:  [1, 2],
+    player: [0, 3]
   });
   
   // Automatically play the loading scene
   Crafty.scene('loading');
 };
-
-// window.onload = function() {
-//   //start crafty
-//   Crafty.init(600, 300);
-//   Crafty.background('rgb(127,127,127)');
-// 
-//   //Paddles
-//   Crafty.e("Paddle, 2D, DOM, Color, Multiway")
-//   	.color('rgb(255,0,0)')
-//   	.attr({ x: 20, y: 100, w: 10, h: 100 })
-//   	.multiway(4, { W: -90, S: 90 });
-//   Crafty.e("Paddle, 2D, DOM, Color, Multiway")
-//   	.color('rgb(0,255,0)')
-//   	.attr({ x: 580, y: 100, w: 10, h: 100 })
-//   	.multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 });
-// 
-//   //Ball
-//   Crafty.e("2D, DOM, Color, Collision")
-//   	.color('rgb(0,0,255)')
-//   	.attr({ x: 300, y: 150, w: 10, h: 10, 
-//   			dX: Crafty.math.randomInt(2, 5), 
-//   			dY: Crafty.math.randomInt(2, 5) })
-//   	.bind('EnterFrame', function () {
-//   		//hit floor or roof
-//   		if (this.y <= 0 || this.y >= 290)
-//   			this.dY *= -1;
-// 
-//   		if (this.x > 600) {
-//   			this.x = 300;
-//   			Crafty("LeftPoints").each(function () { 
-//   				this.text(++this.points + " Points") });
-//   		}
-//   		if (this.x < 10) {
-//   			this.x = 300;
-//   			Crafty("RightPoints").each(function () { 
-//   				this.text(++this.points + " Points") });
-//   		}
-// 
-//   		this.x += this.dX;
-//   		this.y += this.dY;
-//   	})
-//   	.onHit('Paddle', function () {
-//   	this.dX *= -1;
-//   })
-// 
-//   //Score boards
-//   Crafty.e("LeftPoints, DOM, 2D, Text")
-//   	.attr({ x: 20, y: 20, w: 100, h: 20, points: 0 })
-//   	.text("0 Points");
-//   Crafty.e("RightPoints, DOM, 2D, Text")
-//   	.attr({ x: 515, y: 20, w: 100, h: 20, points: 0 })
-//   	.text("0 Points");
-// };
