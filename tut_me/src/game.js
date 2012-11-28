ee243aab7b38966353747fb3592a768c492ccd97
Darren Torpey <darren.torpey@gmail.com>
2012-11-28 14:52:20 -0500

    Automated map generation (just trees for now!)

--- tut_me/src/game.js
+++ tut_me/src/game.js
@@ -26,12 +26,23 @@ var Game = {
 
 		Crafty.background(Game.BACKGROUND_COLOR);
 
-		Crafty.e('Tree').at(0, 0);
-		Crafty.e('Tree').at(0, 1);
-		Crafty.e('Tree').at(1, 0);
-		Crafty.e('Tree').at(3, 2);
+		this.setupMap();
 
 		console.log('-- Game Initialized --');	
+	},
+
+	// Lay out all the pieces of the map
+	setupMap: function() {
+
+		// Trees
+		for (var x = 0; x < this.map_grid.width; x++) {
+			for (var y = 0; y < this.map_grid.height; y++) {
+				var place_it = x == 0 || x == this.map_grid.width - 1 || y == 0 || y == this.map_grid.height - 1 || Math.random() < 0.06
+				if (place_it) {
+				  Crafty.e('Tree').at(x, y);
+				}
+			}
+		}
 	}
 }
 Game.start = Game.start.bind(Game)
