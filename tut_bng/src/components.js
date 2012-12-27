a73c6e60515f51f292c8d9ecf149e7be6ea59e94
Darren Torpey <darren.torpey@gmail.com>
2012-12-27 15:32:46 -0500

    BNG Crafty Tutorial step 3 - PC

--- tut_bng/src/components.js
+++ tut_bng/src/components.js
@@ -42,3 +42,12 @@ Crafty.c('Bush', {
 			.color('rgb(20, 185, 40)');
 	},
 });
+
+// This is the player-controlled character
+Crafty.c('PlayerCharacter', {
+	init: function() {
+		this.requires('Actor, Fourway, Color')
+			.fourway(4)
+			.color('rgb(20, 75, 40)');
+	}
+});
--- tut_bng/src/game.js
+++ tut_bng/src/game.js
@@ -27,6 +27,9 @@ Game = {
 		Crafty.init(Game.width(), Game.height());
 		Crafty.background('rgb(249, 223, 125)');
 
+		// Player character, placed at 5, 5 on our grid
+		Crafty.e('PlayerCharacter').at(5, 5);
+
 		// Place a tree at every edge square on our grid of 16x16 tiles
 		for (var x = 0; x < Game.map_grid.width; x++) {
 			for (var y = 0; y < Game.map_grid.height; y++) {
