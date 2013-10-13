43d7f665c38699fe97f5cd54f1fdb17801303a0c
Darren Torpey <darren.torpey@gmail.com>
2013-10-13 16:50:16 -0400

    Crafty 0.5.4 fix

--- tut_bng/src/scenes.js
+++ tut_bng/src/scenes.js
@@ -79,11 +79,13 @@ Crafty.scene('Victory', function() {
 	// the game when a key is pressed
 	var delay = true;
 	setTimeout(function() { delay = false; }, 5000);
-	this.restart_game = Crafty.bind('KeyDown', function() {
+	this.restart_game = function() {
 		if (!delay) {
 			Crafty.scene('Game');
 		}
-	});
+	};
+
+	this.restart_game = Crafty.bind('KeyDown', this.restart_game);
 }, function() {
 	// Remove our event binding from above so that we don't
 	//  end up having multiple redundant event watchers after
